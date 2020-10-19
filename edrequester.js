7944a99534dba1e81dba4e83b0acfd7752e96e21
Liyud <54576293+liyud-ux@users.noreply.github.com>
2020-10-19 16:07:21 +0200

    Update edrequester.js

--- edrequester.js
+++ edrequester.js
@@ -4,11 +4,14 @@ const readline = require('readline').createInterface({
 input: process.stdin,
 output: process.stdout
 })
+//Asks for EcoleDirecte Username and password
 readline.question('Input Username : ', name => {
 readline.question('Input Password : ', password => {
 api.accounts(name, password)
 .then((account) => {
+//Transforms the JSON into a big pile of strings
 var accountjs = JSON.stringify(account)
+//Logs the big pile of strings and informations
 console.log(accountjs)
 })
 })
